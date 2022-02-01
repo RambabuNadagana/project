179bc14d7c736d8533256cdb095893d8ffe17255
RambabuNadagana <94271381+RambabuNadagana@users.noreply.github.com>
2022-02-01 13:35:00 +0530

    Update creating db and ec2.tf

--- creating db and ec2.tf
+++ creating db and ec2.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "myec2" {
   iam_instance_profile = "ram-s3-role"
   security_groups = ["sg-0bb5391635b3c304e"]
   tags = {
-    Name = "cpms2"
+    Name = "cpms3"
   }
 }
 resource "aws_db_instance" "default" {
@@ -20,7 +20,7 @@ resource "aws_db_instance" "default" {
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
   name                 = "cpms"
-  identifier           = "ramdb"
+  identifier           = "ramdb3"
   username             = "admin"
   password             = "Ramrebel56"
   parameter_group_name = "default.mysql5.7"
