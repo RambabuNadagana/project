b539859f859298c4633708191f88a7d853dd1471
RambabuNadagana <94271381+RambabuNadagana@users.noreply.github.com>
2022-02-01 11:13:53 +0530

    Update creating db and ec2.tf

--- creating db and ec2.tf
+++ creating db and ec2.tf
@@ -20,7 +20,7 @@ resource "aws_db_instance" "default" {
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
   name                 = "cpms"
-  identifier           = "myrdb2"
+  identifier           = "ramdb"
   username             = "admin"
   password             = "Ramrebel56"
   parameter_group_name = "default.mysql5.7"
