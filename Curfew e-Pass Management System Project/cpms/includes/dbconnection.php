6e286272abc34f390e7c45aeba797a5e82969995
RambabuNadagana <94271381+RambabuNadagana@users.noreply.github.com>
2022-02-02 22:04:34 +0530

    Update dbconnection.php

--- Curfew e-Pass Management System Project/cpms/includes/dbconnection.php
+++ Curfew e-Pass Management System Project/cpms/includes/dbconnection.php
@@ -1,9 +1,9 @@
 <?php 
 // DB credentials.
 define('DB_HOST','localhost');
-define('DB_USER','admin');
-define('DB_PASS','Ramrebel56');
-define('DB_NAME','ramdb9');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','cpms');
 // Establish database connection.
 try
 {
